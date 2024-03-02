1018fcd8780fd49ead784351de1368fc8c878b5c
TruongDan01 <danvtpc06576@fpt.edu.vn>
2024-03-02 21:19:13 +0700

    Revert "Add image_url field to categories table"
    This reverts commit 2f2b9b954dd0f0808dea4869475368d3cdcfd080.

--- database/migrations/2024_02_29_124103_create_categories_table.php
+++ database/migrations/2024_02_29_124103_create_categories_table.php
@@ -14,7 +14,6 @@ return new class extends Migration
         Schema::create('categories', function (Blueprint $table) {
             $table->id();
             $table->string('category_name');
-            $table->string('image_url');
             $table->integer('orders');
             $table->tinyInteger('status');
             $table->timestamps();
