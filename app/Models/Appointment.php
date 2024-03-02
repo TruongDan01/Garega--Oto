7b3518e08055323931437c6e7d08b7f09ea1b338
TruongDan01 <danvtpc06576@fpt.edu.vn>
2024-03-02 21:04:10 +0700

    Create Appointment Model

---
+++ app/Models/Appointment.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Appointment extends Model
+{
+    use HasFactory;
+
+    protected $table = 'appointments';
+
+    protected $fillable = [
+        'customer_id',
+        'branch_id',
+        'employee_id',
+        'appointment_date',
+        'status',
+        'orders'
+    ];
+}
