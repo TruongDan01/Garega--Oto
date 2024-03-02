0b6e01a3acc57c1cba332d8343cf02af959d737e
TruongDan01 <danvtpc06576@fpt.edu.vn>
2024-03-02 21:05:25 +0700

    Create Contact Image Model

---
+++ app/Models/ContactImage.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class ContactImage extends Model
+{
+    use HasFactory;
+
+    protected $table = 'contact_images';
+
+    protected $fillable = [
+        'contact_id',
+        'image_url',
+        'orders'
+    ];
+
+    public function contact()
+    {
+        return $this->belongsTo(Contact::class, 'contact_id');
+    }
+}
