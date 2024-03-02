21fb53054b2c3e2e7df5e78df3d0b1960c27aeab
TruongDan01 <danvtpc06576@fpt.edu.vn>
2024-03-02 21:05:42 +0700

    Create Customer Information Model

---
+++ app/Models/CustomerInformation.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class CustomerInformation extends Model
+{
+    use HasFactory;
+
+    protected $table = 'customer_information';
+    protected $fillable = [
+        'name',
+        'email',
+        'phone',
+        'address',
+        'orders',
+        'created_at',
+        'updated_at',
+    ];
+}
