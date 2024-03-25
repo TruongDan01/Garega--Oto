eea31722832471590b7716e654c811aaf5464cad
TruongDan01 <danvtpc06576@fpt.edu.vn>
2024-03-25 16:53:28 +0700

    Create AppointmentControlller

---
+++ app/Http/Controllers/api/AppointmentController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers\api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\Branch;
+use App\Models\Product;
+
+class AppointmentController extends Controller
+{
+    
+}
