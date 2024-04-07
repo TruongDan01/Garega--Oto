fe8fb3206d85d1dfce6d85ae01e83bfc246cc3f1
Nguyễn Minh Triết <145202740+TrietNamePK12345@users.noreply.github.com>
2024-04-07 13:34:50 +0700

    Update Models/Promotion( add protected $date, getDurationAttribute function, getPromotionStatusAttribute function))

--- app/Models/Promotion.php
+++ app/Models/Promotion.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Carbon\Carbon;
 
 class Promotion extends Model
 {
@@ -20,4 +21,46 @@ class Promotion extends Model
         'end_date',
         'orders'
     ];
+
+    protected $dates = ['start_date', 'end_date'];
+    public function product()
+    {
+        return $this->belongsTo(Product::class, 'products_id');
+    }
+
+    public function getDurationAttribute()
+    {
+        if ($this->start_date && $this->end_date) {
+            $start = Carbon::parse($this->start_date);
+            $end = Carbon::parse($this->end_date);
+
+            // Tính khoảng thời gian
+            $diff = $end->diff($start);
+
+            // số ngày và số giờ
+            $days = $diff->days;
+            $hours = $diff->h;
+
+            // phút
+            $minutes = $diff->i;
+            if ($minutes > 0) {
+                $hours++;
+            }
+
+            return $days . ' ngày ' . $hours . ' giờ ' . $minutes . ' phút';
+        }
+        return null;
+    }
+
+    public function getPromotionStatusAttribute()
+    {
+        $now = Carbon::now();
+        if ($now->lt($this->start_date)) {
+            return 'Chưa bắt đầu';
+        } elseif ($now->gte($this->start_date) && $now->lte($this->end_date)) {
+            return 'Còn hạn';
+        } else {
+            return 'Hết hạn';
+        }
+    }
 }
