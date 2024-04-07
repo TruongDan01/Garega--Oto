465ea18639ed4e43ac9df28c6e7a0455a7252000
Nguyễn Minh Triết <145202740+TrietNamePK12345@users.noreply.github.com>
2024-04-07 23:12:49 +0700

    Update AppointmentController (update index function,  update show function)

--- app/Http/Controllers/api/AppointmentController.php
+++ app/Http/Controllers/api/AppointmentController.php
@@ -10,36 +10,24 @@ use App\Models\Appointment;
 use App\Models\EmployeeSchedule;
 use App\Http\Resources\BranchResource;
 use Illuminate\Http\Request;
+use App\Http\Resources\AppointmentResource;
 
 class AppointmentController extends Controller
 {
     public function index()
     {
         $appointments = Appointment::all();
+        $appointmentsData = $appointments->map->getAppointmentDetails();
+        return $appointmentsData;
 
-        foreach ($appointments as $appointment) {
-
-            $branch = Branch::find($appointment->branch_id);
-            $appointment->branch_name = $branch->name;
-            $appointment->branch_address = $branch->address;
-
-            if ($appointment->status == 1) {
-                $appointment->status_name = 'Đang xác nhận';
-            } else if ($appointment->status == 2) {
-                $appointment->status_name = 'Đã xác nhận';
-            } else {
-                $appointment->status_name = 'Chưa xác nhận';
-            }
-        }
-
-        return response()->json($appointments);
     }
 
     public function show($id)
     {
         $appointment = Appointment::findOrFail($id);
-        return response()->json($appointment);
+        return new AppointmentResource($appointment);
     }
+
     public function getBranchDetails($branchId, $userId)
     {
         $branch = Branch::find($branchId);
