40d2a6316d35ebca320b28f3421e35337c6e616a
Nguyễn Minh Triết <145202740+TrietNamePK12345@users.noreply.github.com>
2024-04-07 15:50:10 +0700

    Create api route get users to connect with show fucntion in UserController

--- routes/api.php
+++ routes/api.php
@@ -6,6 +6,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\api\HomeController;
 use App\Http\Controllers\api\ServiceController;
 use App\Http\Controllers\api\BranchController;
+use App\Http\Controllers\api\UserController;
 
 /*
 |--------------------------------------------------------------------------
@@ -27,6 +28,8 @@ Route::apiResources([
     'services' => ServiceController::class,
     'branches' => BranchController::class,
     'appointments' => AppointmentController::class,
+    'users' => UserController::class,
 ]);
 Route::get('services/category/{serviceType}', [ServiceController::class, 'getServicesByCategory']);
 Route::get('appointment/create/{branchId}/{userId}', [AppointmentController::class, 'getBranchDetails']);
+Route::get('users/show-information/{userId}', [UserController::class, 'show']);
