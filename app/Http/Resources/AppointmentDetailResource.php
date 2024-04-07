650991ec737a5b69c059a6276e77a1d2e5cc332f
Nguyễn Minh Triết <145202740+TrietNamePK12345@users.noreply.github.com>
2024-04-07 23:17:45 +0700

    Update AppointmentDetailResource (Delete value on toArray))

--- app/Http/Resources/AppointmentDetailResource.php
+++ app/Http/Resources/AppointmentDetailResource.php
@@ -17,13 +17,7 @@ class AppointmentDetailResource extends JsonResource
     public function toArray(Request $request): array
 {
     return [
-        'id' => $this->id,
-        'appointment_id' => $this->appointment_id,
-        'qr_code' => $this->qr_code,
-        'status' => $this->status,
-        'start_time' => Carbon::parse($this->start_time)->format('Y-m-d H:i'),
-        'end_time' => Carbon::parse($this->end_time)->format('Y-m-d H:i'),
-        'orders' => $this->orders,
+
     ];
 }
 }
