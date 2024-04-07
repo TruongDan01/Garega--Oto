887585f432e1e62597dfda50acec7865ee292a93
Nguyễn Minh Triết <145202740+TrietNamePK12345@users.noreply.github.com>
2024-04-07 13:32:57 +0700

    Create Resource/PromotionResrouce and add value of Promotions

---
+++ app/Http/Resources/PromotionResource.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class PromotionResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'name' => $this->name,
+            'description' => $this->description,
+            'start_date' => $this->start_date,
+            'end_date' => $this->end_date,
+            'duration' => $this->duration,
+            'promotion_status' => $this->promotion_status,
+            'product_image' => $this->product->image_url,
+        ];
+    }
+}
