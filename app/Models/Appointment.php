adf527e22f81cf71564db04e27022554be9adb10
Nguyễn Minh Triết <145202740+TrietNamePK12345@users.noreply.github.com>
2024-04-07 23:15:11 +0700

    Update AppointmentModel add branch, customer, employee function

--- app/Models/Appointment.php
+++ app/Models/Appointment.php
@@ -4,6 +4,9 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\Branch;
+use App\Models\CustomerInformation;
+use App\Models\User;
 
 class Appointment extends Model
 {
@@ -23,5 +26,48 @@ class Appointment extends Model
         'updated_at'
     ];
 
- 
+    public function branch()
+    {
+        return $this->belongsTo(Branch::class, 'branch_id');
+    }
+
+    public function customer()
+    {
+        return $this->belongsTo(CustomerInformation::class, 'customer_id');
+    }
+
+    public function employee()
+    {
+        return $this->belongsTo(User::class, 'employee_id');
+    }
+
+    public function getAppointmentDetails()
+    {
+        return [
+            'id' => $this->id,
+            'appointment_date' => $this->appointment_date,
+            'status' => $this->getStatusText(),
+            'branch' => [
+                'name' => $this->branch->name,
+                'address' => $this->branch->address,
+            ],
+        ];
+    }
+
+    private function getStatusText()
+    {
+        switch ($this->status) {
+            case 0:
+                return 'Hủy bỏ';
+            case 1:
+                return 'Chờ xác nhận';
+            case 2:
+                return 'Đã xác nhận';
+            case 3:
+                return 'Hoành thành';
+            default:
+                return 'Trạng thái không xác định';
+        }
+    }
+
 }
