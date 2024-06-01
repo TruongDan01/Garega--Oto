c873459d8193a017a52560760bb31f1083d202bb
trietnmpc06220 <145202740+TrietNamePK12345@users.noreply.github.com>
2024-06-01 15:51:22 +0700

    Update routes\api.php

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,13 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProductController;
+use App\Http\Controllers\PromotionController;
+use App\Http\Controllers\BranchController;
+use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\AppointmentController;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\EmployeeController;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,3 +24,28 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+    // ======= category ======= //
+Route::apiResource('categories', CategoryController::class)->only(['index', 'show']);
+
+    // ======= products ======= //
+Route::apiResource('products', ProductController::class)->only(['index', 'show']);
+
+    // ======= branch ======= //
+Route::apiResource('branches', BranchController::class)->only(['index', 'show']);
+
+    // ======= promotion ======= //
+Route::apiResource('promotions', PromotionController::class)->only(['index', 'show']);
+
+    // ======= All Appointment by Customer ======= //
+Route::get('allAppointment/customer_id={user_id}', [AppointmentController::class, 'getAllAppointmentsByUserId']);
+
+    // ======= Get All Employee By Branch Id ======= //
+Route::get('allEmployee/branch_id={id}', [UserController::class, 'getAllEmployeeByBranchId']);
+
+    // ======= Get All TimeSlot By User Id ======= //
+Route::get('allTimeSlot/user_id={id}', [EmployeeController::class, 'getAllTimeSlotByEmployeeId']);
+
+//    // ======= Insert Appointment and AppointmentDetail ======= //
+//Route::apiResource('appointment', AppointmentController::class)->only(['index','show','store']);
+
