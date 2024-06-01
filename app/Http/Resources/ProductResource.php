0f19f530f0b1642b5fbfa0d317dec66e32445a15
trietnmpc06220 <145202740+TrietNamePK12345@users.noreply.github.com>
2024-06-01 15:55:29 +0700

    Create ProductResource

---
+++ app/Http/Resources/ProductResource.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+use Illuminate\Http\Resources\Json\ResourceCollection;
+
+class ProductResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'id' => $this->id,
+            'name' => $this->name,
+            'description' => $this->description,
+            'price' => $this->price,
+            'image_url' => $this->image_url,
+            'category_id' => $this->category_id,
+            'status' => $this->status == 1 ? 'Đang hoạt động' : 'Ngừng hoạt động',
+        ];
+    }
+}
