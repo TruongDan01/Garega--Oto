06d68b000c804d5a34126d5639a3d1af9c4b024c
trietnmpc06220 <145202740+TrietNamePK12345@users.noreply.github.com>
2024-06-01 15:54:00 +0700

    Create ProductController

---
+++ app/Http/Controllers/ProductController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use App\Http\Resources\ProductResource;
+use App\Model\Product;
+use Illuminate\Http\Request;
+
+class ProductController extends Controller
+{
+    public function index()
+    {
+        return ProductResource::collection(Product::all());
+    }
+
+    public function show($id)
+    {
+        return new ProductResource(Product::findOrFail($id));
+    }
+}
