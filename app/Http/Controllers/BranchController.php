065511dd5aa0dd863a62eccc0602a181c15bf792
trietnmpc06220 <145202740+TrietNamePK12345@users.noreply.github.com>
2024-06-07 11:40:33 +0700

    Update BranchController

--- app/Http/Controllers/BranchController.php
+++ app/Http/Controllers/BranchController.php
@@ -4,7 +4,9 @@ namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
 use App\Http\Resources\BranchResource;
+use App\Http\Resources\EmployeeResource;
 use App\Model\Branch;
+use App\Model\Employee;
 use Illuminate\Http\Request;
 
 class BranchController extends Controller
@@ -13,10 +15,4 @@ class BranchController extends Controller
     {
         return BranchResource::collection(Branch::all());
     }
-
-    public function show($id)
-    {
-        return new BranchResource(Branch::findOrFail($id));
-    }
-
 }
