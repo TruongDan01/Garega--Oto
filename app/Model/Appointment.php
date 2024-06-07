2b8134a69481cd436973b02eebeaa4914772197c
trietnmpc06220 <145202740+TrietNamePK12345@users.noreply.github.com>
2024-06-07 11:36:13 +0700

    Update Appointment Model

--- app/Model/Appointment.php
+++ app/Model/Appointment.php
@@ -11,43 +11,25 @@ class Appointment extends Model
 {
     use HasFactory;
     protected $fillable = [
-      'customer_id',
+      'id',
+      'zalo_id',
       'branch_id',
       'employee_id',
       'appointment_date',
-      'notes',
       'status'
     ];
 
-    public function customer()
-    {
-        return $this->belongsTo(Customer::class);
-    }
     public function branch()
     {
         return $this->belongsTo(Branch::class);
     }
     public function employee()
     {
-        return $this->belongsTo(User::class, 'employee_id');
+        return $this->belongsTo(User::class);
     }
 
-    // cần sử duụng bên AppointmentResource : status => $this->status_text;
-    public function getStatusTextAttribute()
+    public function appointmentDetail()
     {
-        switch ($this->status) {
-            case 0:
-                return 'Chờ xác nhận';
-            case 1:
-                return 'Đã xác nhận';
-            case 2:
-                return 'Đã hủy';
-            default:
-                return 'Chưa có trạng thái';
-        }
-    }
-    public function getFormattedDateAttribute()
-    {
-        return Carbon::parse($this->appointment_date)->format('H:i:s d-m-Y');
+        return $this->hasMany(AppointmentDetail::class);
     }
 }
