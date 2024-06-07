e7eebc3e6231e954d86e6757462f6e0677142f07
trietnmpc06220 <145202740+TrietNamePK12345@users.noreply.github.com>
2024-06-07 11:40:55 +0700

    Update AppointmentController

--- app/Http/Controllers/AppointmentController.php
+++ app/Http/Controllers/AppointmentController.php
@@ -3,32 +3,77 @@
 namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
+use App\Http\Resources\AppointmentDetailResource;
 use App\Http\Resources\AppointmentResource;
 use App\Model\Appointment;
+use App\Model\AppointmentDetail;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class AppointmentController extends Controller
 {
-    public function index()
+    public function getAllAppointmentsByZaloId($id)
     {
-        return AppointmentResource::collection(Appointment::all());
-    }
-
-    public function show($id)
-    {
-        return new AppointmentResource(Appointment::findOrFail($id));
-    }
-
-    public function store()
-    {
-        $appointment = Appointment::create();
-        return new AppointmentResource($appointment);
-    }
-
-    public function getAllAppointmentsByUserId($userId)
-    {
-        $appointments = Appointment::where('customer_id', $userId)->get();
-
+        $appointments = Appointment::where('zalo_id', $id)->get();
         return AppointmentResource::collection($appointments);
     }
+
+    public function add(Request $request)
+    {
+        DB::beginTransaction();
+        try {
+            $appointment = Appointment::create([
+                'zalo_id' => $request->zalo_id,
+                'branch_id' => $request->branch_id,
+                'employee_id' => $request->employee_id,
+                'appointment_date' => $request->appointment_date,
+                'status' => $request->status
+            ]);
+            $appointmentDetailsData = $request->appointment_details;
+            foreach ($appointmentDetailsData as $detail) {
+                AppointmentDetail::create([
+                    'appointment_id' => $appointment->id,
+                    'product_id' => $detail['product_id'],
+                    'time_picker_id' => $detail['time_picker_id'],
+                    'customer_name' => $detail['customer_name'],
+                    'customer_email' => $detail['customer_email'],
+                    'customer_address' => $detail['customer_address'],
+                    'customer_phone' => $detail['customer_phone'],
+                    'customer_note' => $detail['customer_note']
+                ]);
+            }
+            DB::commit();
+            return new AppointmentResource($appointment);
+
+        } catch (\Exception $e) {
+            DB::rollBack();
+            return response()->json([
+                'message' => 'Something went really wrong'
+            ], 500);
+        }    }
+
+    public function update(Request $request, $id)
+    {
+        try {
+            $appointment = Appointment::findOrFail($id);
+            $appointment->update([
+                'status' => $request->status,
+            ]);
+            return response()->json([
+                'message' => "Appointment successfully updated"
+            ], 200);
+
+        } catch (\Exception $e) {
+            return response()->json([
+                'message' => 'Something went really wrong'
+            ], 500);
+        }
+    }
+
+    public function detail($id)
+    {
+        $appointmentDetail = AppointmentDetail::where('appointment_id', $id)->get();
+        return AppointmentDetailResource::collection($appointmentDetail);
+    }
+
 }
