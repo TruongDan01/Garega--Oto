521b9dce2b22edfcb5b88f1f0710c7bbed0c2b52
trietnmpc06220 <145202740+TrietNamePK12345@users.noreply.github.com>
2024-06-07 11:32:00 +0700

    Update Rating Model
    change Review Model ==> Rating Model

--- app/Model/Rating.php
+++ app/Model/Rating.php
@@ -5,15 +5,15 @@ namespace App\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Review extends Model
+class Rating extends Model
 {
     use HasFactory;
     protected $fillable = [
       'appointment_id',
-      'rating',
-      'comment',
-      'status'
+      'rating_value',
+      'rating_status',
     ];
+
     public function appointment()
     {
         return $this->belongsTo(Appointment::class);
