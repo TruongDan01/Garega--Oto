89e004e8bc4f82159d12cb09d497a42f1c982ced
trietnmpc06220 <145202740+TrietNamePK12345@users.noreply.github.com>
2024-06-11 12:09:01 +0700

    Update AppointmentController

--- app/Http/Controllers/AppointmentController.php
+++ app/Http/Controllers/AppointmentController.php
@@ -7,6 +7,7 @@ use App\Http\Resources\AppointmentDetailResource;
 use App\Http\Resources\AppointmentResource;
 use App\Model\Appointment;
 use App\Model\AppointmentDetail;
+use App\Model\Product;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 
@@ -29,11 +30,26 @@ class AppointmentController extends Controller
                 'appointment_date' => $request->appointment_date,
                 'status' => $request->status
             ]);
+
             $appointmentDetailsData = $request->appointment_details;
             foreach ($appointmentDetailsData as $detail) {
+                $productIds = $detail['product_id'];
+                foreach ($productIds as $productId) {
+                    // Kiểm tra xem product_id có tồn tại trong bảng products không
+                    $existingProduct = Product::where('id', $productId)->exists();
+                    if (!$existingProduct) {
+                        // Nếu product_id không tồn tại, báo lỗi
+                        DB::rollBack();
+                        return response()->json([
+                            'message' => 'Dịch vụ với id ' . $productId . ' không tồn tại!'
+                        ], 400);
+                    }
+                }
+
+                // Nếu tất cả các product_id đều tồn tại, tiếp tục tạo chi tiết hẹn
                 AppointmentDetail::create([
                     'appointment_id' => $appointment->id,
-                    'product_id' => $detail['product_id'],
+                    'product_id' => json_encode($productIds),
                     'time_picker_id' => $detail['time_picker_id'],
                     'customer_name' => $detail['customer_name'],
                     'customer_email' => $detail['customer_email'],
@@ -50,7 +66,9 @@ class AppointmentController extends Controller
             return response()->json([
                 'message' => 'Something went really wrong'
             ], 500);
-        }    }
+        }
+    }
+
 
     public function update(Request $request, $id)
     {
