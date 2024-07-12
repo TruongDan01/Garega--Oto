1828d7407a94ae6f897919dddc056a2329bbab03
trietnmpc06220 <145202740+TrietNamePK12345@users.noreply.github.com>
2024-07-13 06:43:02 +0700

    Update AppointmentDetailResource.php

--- app/Http/Resources/AppointmentDetailResource.php
+++ app/Http/Resources/AppointmentDetailResource.php
@@ -17,12 +17,16 @@ class AppointmentDetailResource extends JsonResource
         return [
             'appointment_id' => $this->appointment_id,
             'product_id' => $this->product_id,
+            'employee_id' => $this->appointment->employee_id,
+            'branch_id' => $this->appointment->branch_id,
             'time_picker_id' => $this->time_picker_id,
+            'appointment_date' => $this->appointment->appointment_date,
             'customer_name' => $this->customer_name,
             'customer_email' => $this->customer_email,
             'customer_address' => $this->customer_address,
             'customer_phone' => $this->customer_phone,
             'customer_note' => $this->customer_note,
+            'status' => $this->appointment->status,
         ];
     }
 }
